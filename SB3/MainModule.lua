5c8252e6e1ffcdf905d5053d2471f71e8174e297
LuaStackin <161803882+LuaStackin@users.noreply.github.com>
2024-03-02 17:25:37 +0000

    Update MainModule.lua

--- SB3/MainModule.lua
+++ SB3/MainModule.lua
@@ -326,6 +326,7 @@ MainModule.CreateTimer = function(Time, Function)
       return Timer
    end
    coroutine.resume(Thread)
+   warn("Timer Will Trigger Function in", tostring(60 * Time), "Minute(s)")
    return ResetTimer, Thread
 end
       
