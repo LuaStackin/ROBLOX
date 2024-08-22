c80be11c8ff93c50257433574ccee7b3cd258f17
LuaStackin <161803882+LuaStackin@users.noreply.github.com>
2024-08-22 11:31:18 +0100

    Update CheckLimiteds_V3.lua

--- BB/V2/Modules/CheckLimiteds_V3.lua
+++ BB/V2/Modules/CheckLimiteds_V3.lua
@@ -97,6 +97,9 @@ local _MinimumPrice_ = function(v)
 end
 
 
--- Hi, this is just testing not really useful.
+--[[
+    NOTE: This script is unfinished, neither was it made for a malicious purpose. It was only made to grab the RAP of certain items and publish them to a discord webhook for me to see, 
+    basically just an item sniper without the automation, if you use this don't expect anything flashy or for it to be automated.
+]]--
 warn(string.format("Script Version: V%s", tostring(Version)))
 return _Main_, _SetHook_, _ServerHop_, _UnderRapOnly_, _MinimumPrice_
