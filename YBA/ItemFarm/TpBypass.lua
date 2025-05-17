d2f8cba9f9fb6b1e527a2d619cba8156ff20cbe3
LuaStackin <161803882+LuaStackin@users.noreply.github.com>
2025-05-17 03:36:36 +0100

    Update TpBypass.lua

--- YBA/ItemFarm/TpBypass.lua
+++ YBA/ItemFarm/TpBypass.lua
@@ -2,7 +2,17 @@ local TPBypass
 TPBypass = hookfunction(getrawmetatable(game).__namecall, newcclosure(function(self, ...)
      local args = {...}
      if self.Name == "Returner" and args[1] == "idklolbrah2de"  then
+         --
          return "  ___XP DE KEY"
+     end
+     if getnamecallmethod() == 'FireServer' then
+         --
+         if args[1] == 'SigL' then
+             --
+             warn('Ban Attempt Prevented - SigL');
+             return
+         end
+         --
      end
      return TPBypass(self, ...)
 end))
